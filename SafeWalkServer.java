a2f1a8d3ec654b7245bece60365d427e1162f73a
wfinger <bfinger44@pal-nat184-076-150.itap.purdue.edu>
2014-11-14 17:32:15 -0500

    added checkValidityRequest

--- SafeWalkServer.java
+++ SafeWalkServer.java
@@ -53,7 +53,9 @@ public class SafeWalkServer implements Runnable {
             try {
             socket = serverSocket.accept();
             System.out.printf("Connection received from %s%n", socket);
-            System.out.println("Received from client: " + getInput());
+            String input = getInput();
+            System.out.println("Received from client: " + input);
+            System.out.println(isCommand(input));
             }
             catch (Exception e) {
                 e.printStackTrace();
@@ -70,11 +72,46 @@ public class SafeWalkServer implements Runnable {
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         pw.println("Enter a command or request: ");
         pw.flush();
-        
         String input = in.readLine();
         return input;
     }
     
-}
+    private boolean isCommand(String input) {
+        if (input.charAt(0) == ':') 
+            return true;
+        return false;
+    }
+    
+//    private void checkValidityCommand(String input) {
+//        if (input.equals(":LIST_PENDING_REQUESTS"))
+//            // CALL THE METHOD
+//        else if (input.equals(":RESET"))
+//            // CALL THE METHOD
+//        else if (input.equals(":SHUTDOWN"))
+//            // Close the calling client and also any other open client
+//        
+//        else
+//            return false;
+//    }
+    
+    private void checkValidityRequest(String input) {
+        String[] locations = {"CL50", "EE", "LWSN", "PMU", "PUSH", "*"};
+        char[] charArray = input.toCharArray();
+        int[] commaIndex = new int[3];
+        int c = 0;
+        for (int i = 0; i < charArray.length; i++) {
+            if (charArray[i] == ',') {
+                commaIndex[c] = i;
+                c++;
+            }
+        }
+        String token1 = input.substring(0, commaIndex[0]);
+        String token2 = input.substring(commaIndex[0], commaIndex[1]);
+        String token3 = input.substring(commaIndex[1], commaIndex[2]);
+        String token4 = input.substring(commaIndex[2], input.length());   
+    }
         
+      
             
+    
+}
